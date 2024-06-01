827858d91e5124d1943534a46f21a590944eb508
Carl Bäckström <genzorg@gmail.com>
2024-06-01 10:01:37 -0600

    chore: package update and added README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# node-express-mock
+
+This is a super simple Node.js server that is meant to host JSON sample data
+
+## Usage
+
+```bash
+npm start
+curl http://localhost:3000/categories
+```
--- src/index.js
+++ src/index.js
@@ -1,43 +1,45 @@
+const express = require("express");
 
-const express = require('express')
-
-const port = 3000
-
-// app.get('/success', (req, res) => {
-//   return res.json({result: 'test'});
-// })
-
-// app.get('/token_expired', (req, res) => {
-//   res.status(401).json({error: 'refresh_token expired'});
-// })
-
-// app.post('/restapi/v2.1/accounts/c967a94d-f345-4ffc-9e0e-2e5e79291af9/envelopes', (req, res) => {
-//   res.status(401).json({error: 'refresh_token expired'});
-// })
+const port = 3000;
 
 const routes = [
-  {method: 'GET', route: "/categories", reply: [{id:1, 'title': 'Category1'}], status: 200},
+  {
+    method: "GET",
+    route: "/categories",
+    reply: [{ id: 1, title: "Category1" }],
+    status: 200,
+  },
+  {
+    method: "POST",
+    route: "/v2/documents/new",
+    reply: {
+      error_type: "document_action_forbidden",
+      error_message:
+        "You do not have enough document tokens to perform this action on the document.",
+      http_code: 403,
+      error_details: null,
+    },
+    status: 403,
+  },
 ];
 
-function createApp () {  
-  const app = express()
-  routes.forEach(route=> {
-    app.all(route.route, (req, res) => {      
-      console.log(route);
-      if (route.method === 'GET' && req.method === 'GET') {        
+function createApp() {
+  const app = express();
+  routes.forEach((route) => {
+    app.all(route.route, (req, res) => {
+      if (route.method === "GET" && req.method === "GET") {
         res.status(route.status).json(route.reply).end();
-      }      
-    })    
-  })
-  return app
+      }
+      if (route.method === "POST" && req.method === "POST") {
+        res.status(route.status).json(route.reply).end();
+      }
+    });
+  });
+  return app;
 }
-
-
 
 const app = createApp();
 
-
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
